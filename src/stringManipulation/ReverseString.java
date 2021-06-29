44eda0596cec3c639967786a2b60ccec8ea2ba13
piyupaul789 <piyupaul789@gmail.com>
2021-06-30 03:16:46 +0530

    feat(app): Updated reverse string code

---
+++ src/stringManipulation/ReverseString.java
@@ -0,0 +1,22 @@
+package stringManipulation;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String reversed = "";
+		// Scanner sc = new Scanner(System.in);
+		// System.out.println("Please enter word");
+		// String givenStr = sc.nextLine();
+		String givenStr = "Hello world this is Piyu";
+
+		String[] arr = givenStr.split(" ");
+		for (int i = arr.length -1; i >= 0; i--) {
+			reversed += arr[i] + " ";
+
+		}
+
+		System.out.println("Reversed word - " + reversed);
+	}
+
+}
